50500a24093939b012c6bc74a0831cfa969d1d4e
Goryachenok <goryachev.na72@gmail.com>
2022-08-25 00:04:35 +0500

    for first tests

---
+++ temp/mocksData.js
@@ -0,0 +1,81 @@
+'head': {
+    defaults: {
+
+    }
+}
+
+
+
+
+,
+
+__iconsData: {
+    
+        'Arrow - Down 2': {
+            width: '24px',
+            height: '24px'
+        },
+    
+        'arrow': {
+            width: '24px',
+            height: '24px'
+        },
+    
+        'close': {
+            width: '12px',
+            height: '12px'
+        },
+    
+        'empty-wallet': {
+            width: '24px',
+            height: '24px'
+        },
+    
+        'ik': {
+            width: '24px',
+            height: '24px'
+        },
+    
+        'up': {
+            width: '13px',
+            height: '15px'
+        },
+    
+        'watsup': {
+            width: '23px',
+            height: '23px'
+        },
+    
+        'ик': {
+            width: '24px',
+            height: '24px'
+        },
+    
+        'обычная': {
+            width: '52px',
+            height: '52px'
+        },
+    
+},
+
+__pages: [{
+                name: '!main',
+                href: '!main.html',
+                desc: 'Главная',
+             },{
+                name: '404',
+                href: '404.html',
+                desc: '404',
+             },{
+                name: 'index',
+                href: 'index.html',
+                desc: '//',
+             },{
+                name: 'modal',
+                href: 'modal.html',
+                desc: 'Modal',
+             },{
+                name: 'page',
+                href: 'page.html',
+                desc: 'Текстовая страница',
+             }]
---
+++ temp/symbols-data-template.js
@@ -0,0 +1,48 @@
+__iconsData: {
+    
+        'Arrow - Down 2': {
+            width: '24px',
+            height: '24px'
+        },
+    
+        'arrow': {
+            width: '24px',
+            height: '24px'
+        },
+    
+        'close': {
+            width: '12px',
+            height: '12px'
+        },
+    
+        'empty-wallet': {
+            width: '24px',
+            height: '24px'
+        },
+    
+        'ik': {
+            width: '24px',
+            height: '24px'
+        },
+    
+        'up': {
+            width: '13px',
+            height: '15px'
+        },
+    
+        'watsup': {
+            width: '23px',
+            height: '23px'
+        },
+    
+        'ик': {
+            width: '24px',
+            height: '24px'
+        },
+    
+        'обычная': {
+            width: '52px',
+            height: '52px'
+        },
+    
+}
